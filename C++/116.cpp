75373e4976a578b47c07bb1c3b451383a25d18a8
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-25 16:34:47 +0200

    ¡Hola Mundo!

---
+++ C++/116.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 116 - Hola Mundo - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+    int a = 0;
+    cin >> a;
+    for(int i=0; i<a; i++) {
+        cout << "Hola mundo." << endl;
+    }
+    return 0;
+}
