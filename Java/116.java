c6cef29af1cd96d21a2f9624b8a235543b278a8f
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-25 16:39:44 +0200

    ¡Hola Mundo!

---
+++ Java/116.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 116 - Hola Mundo - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int bucle = sc.nextInt();
+        for (int i = 1; i <= bucle; i++) {
+            System.out.println("Hola mundo.");
+        }
+
+    }
+
+}
