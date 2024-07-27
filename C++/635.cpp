a9056729d5cfef13bd9de7dca03ae39967381f62
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 00:11:42 +0200

    Cinquecento

---
+++ C++/635.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// 635 - Cinquecento - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+
+int main()
+{
+    int loop = 0;
+    cin >> loop;
+    for (int i = 0; i<loop; i++) {
+        int year = 0;
+        cin >> year;
+        cout << (year+99)/100 << endl;
+
+    }
+
+}
