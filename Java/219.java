d96d53c8cd55322e43c44ffc0cf21e53bc5c1815
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-27 23:35:18 +0200

    La Lotería de la Peña Atlética

---
+++ Java/219.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 219 - La Lotería de la Peña Atlética - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int casos = sc.nextInt();
+        for (int i = 1; i<=casos; i++) {
+            int numeros = sc.nextInt();
+            int puntuaje = 0;
+            for (int j = 1; j<=numeros; j++) {
+                int numerito = sc.nextInt();
+                if (numerito % 2 == 0) {
+                    puntuaje++;
+                }
+            }
+            System.out.println(puntuaje);
+        }
+    }
+
+}
