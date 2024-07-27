2269f3201dd65623a2279c9f0d93d3d8c76a90c7
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 00:29:10 +0200

    Palabras Pentavocálicas

---
+++ Java/300.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * 300 - Palabras Pentavocálicas - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int prueba = sc.nextInt();
+        sc.nextLine();
+        for (int i = 1; i <= prueba; i++) {
+            String frase = sc.nextLine();
+
+            if (pentavolica(frase)) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    public static boolean pentavolica(String frase) {
+        boolean a = false;
+        boolean e = false;
+        boolean i = false;
+        boolean o = false;
+        boolean u = false;
+        int j = 0;
+        while (j < frase.length() && (!a || !e || !i || !o || !u)) {
+            if (frase.charAt(j) == 'a') {
+                a = true;
+            } else if (frase.charAt(j) == 'e') {
+                e = true;
+            } else if (frase.charAt(j) == 'i') {
+                i = true;
+            } else if (frase.charAt(j) == 'o') {
+                o = true;
+            } else if (frase.charAt(j) == 'u') {
+                u = true;
+            }
+            j++;
+        }
+        return (a) && (e) && (i) && (o) && (u);
+    }
+}
