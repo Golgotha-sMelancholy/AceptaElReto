930ac096302a8c73df7f4251da7ef8dfd83ad0b5
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-27 15:43:04 +0200

    Último Dígito del Factorial

---
+++ Java/114.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 114 - Último Dígito del Factorial - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int casos = sc.nextInt();
+        for (int i = 1; i <= casos; i++) {
+            int numero = sc.nextInt();
+            if (numero >= 5) {
+                System.out.println("0");
+            } else if (numero == 4) {
+                System.out.println("4");
+            } else if (numero == 3) {
+                System.out.println("6");
+            } else if (numero == 2) {
+                System.out.println("2");
+            } else if (numero == 1 || numero == 0) {
+                System.out.println("1");
+            }
+        }
+        System.exit(0);
+    }
+
+}
