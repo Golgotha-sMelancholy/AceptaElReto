537850838082e12c49f7be18e054e049c4279aeb
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 10:29:01 +0200

    Las Calorías

---
+++ Java/363.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 363 - Las Calorías - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int restador = sc.nextInt();
+        while (restador > 0) {
+            int contador = 0;
+            int casos = sc.nextInt();
+            int suma = 0;
+            for (int i = 0; i < casos; i++) {
+                suma += sc.nextInt();
+            }
+            while (suma > 0) {
+                suma -= restador;
+                contador++;
+            }
+            System.out.println(contador);
+            restador = sc.nextInt();
+        }
+    }
+
+}
