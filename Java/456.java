dbd3dc078738efc4d780fca80ad042c5063ceff7
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 11:08:30 +0200

    Tarta Sácher

---
+++ Java/456.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 456 - Tarta Sácher - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pruebas = sc.nextInt();
+        for (int i = 1; i <= pruebas; i++) {
+            int contador = 1;
+            boolean booleano = false;
+            int ancho = sc.nextInt();
+            int alto = sc.nextInt();
+            int limite = sc.nextInt();
+            int anchoalto = ancho * alto;
+            while (!booleano) {
+                if (anchoalto >= limite) {
+                    System.out.println(contador);
+                    booleano = true;
+                } else {
+                    contador++;
+                    anchoalto += ancho * alto;
+                }
+            }
+        }
+
+    }
+
+}
