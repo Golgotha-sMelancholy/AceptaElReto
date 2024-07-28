4731e61ec093792aebaed0b02d474d4a056ec1ac
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 12:17:56 +0200

    Cucharadas de Sopa

---
+++ Java/557.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 557 - Cucharadas de Sopa - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int casos = sc.nextInt();
+        for (int i = 1; i <= casos; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            System.out.println((Math.min(c - a + 1, b + 1)));
+        }
+    }
+
+}
