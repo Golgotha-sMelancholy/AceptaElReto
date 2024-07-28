07076e8590d0aa9e42a0e19b417331b484516d02
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 13:03:44 +0200

    Ordenando el Armario

---
+++ Java/608.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 608 - Ordenando el Armario - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cantidad = sc.nextInt();
+        while (cantidad != 0) {
+            int alarma = 0;
+            boolean distancia = false;
+            for (int i = 1; i <= cantidad; i++) {
+                int grados = sc.nextInt();
+                if (grados <= 4 && !distancia) {
+                    alarma++;
+                    distancia = true;
+                } else if (grados > 6) {
+                    distancia = false;
+                }
+            }
+            System.out.println(alarma);
+            alarma = 0;
+            distancia = false;
+            cantidad = sc.nextInt();
+        }
+    }
+}
