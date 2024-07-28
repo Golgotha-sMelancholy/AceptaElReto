cdb1b06141237fe1cda14dacb970ed2ba005f22f
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 10:58:02 +0200

    Yo Soy Tu...

---
+++ Java/427.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 427 - Yo Soy Tu... - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int casos = sc.nextInt();
+        for (int i = 1; i <= casos; i++) {
+            String nombre = sc.next();
+            String parentesco = sc.next();
+            if (nombre.equals("Luke") && parentesco.equals("padre")) {
+                System.out.println("TOP SECRET");
+            } else {
+                System.out.println(nombre + ", yo soy tu " + parentesco);
+            }
+        }
+    }
+}
