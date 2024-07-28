361a3355a7ef478d7f81fc786276fcaae472c435
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 10:30:43 +0200

    Espionaje en Navidad

---
+++ Java/364.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 364 - Espionaje en Navidad - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String frase = sc.nextLine();
+        while (!frase.equals("FIN")) {
+            int i = 0;
+            String abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            while (frase.length() != i) {
+                int encriptacion = 1 + abecedario.indexOf(frase.charAt(i));
+                if (abecedario.indexOf(frase.charAt(i)) != -1) {
+                    System.out.print(encriptacion == abecedario.length() ? 'A' : abecedario.charAt(encriptacion));
+                } else {
+                    System.out.print(" ");
+                }
+                i++;
+            }
+            System.out.println();
+            frase = sc.nextLine();
+        }
+    }
+}
