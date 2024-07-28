d2316cb5e6262c13bac750c11527750218084da6
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 11:26:28 +0200

    Tensión Descompensada

---
+++ Java/506.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 506 - Tensión Descompensada - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int prueba = sc.nextInt();
+        for (int i = 1; i <= prueba; i++) {
+            int valor = sc.nextInt();
+            char division = sc.next().charAt(0);
+            int valor2 = sc.nextInt();
+
+            if (valor >= valor2) {
+                System.out.println("BIEN");
+            } else {
+                System.out.println("MAL");
+            }
+        }
+
+    }
+}
