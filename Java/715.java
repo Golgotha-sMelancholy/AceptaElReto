d6c1139461e0d08037f87f7996a77db782b27c30
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 13:26:04 +0200

    ¿Hay Suficientes?

---
+++ Java/715.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 715 - ¿Hay Suficientes? - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pruebas = sc.nextInt();
+        for (int i = 1; i<=pruebas; i++) {
+            int uvas = sc.nextInt();
+            int personas = sc.nextInt();
+            if (uvas / personas >= 12) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+}
