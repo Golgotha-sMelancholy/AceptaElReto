902589f307fd167c9c8fb3f8cad3463e7ed1f6e7
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 13:03:04 +0200

    Ordenando el Armario

---
+++ Java/605.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 605 - Ordenando el Armario - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char armario = sc.next().charAt(0);
+        while (armario != '.') {
+            int invierno = 0;
+            int verano = 0;
+            int entretiempo = 0;
+            while (armario != ('.')) {
+                if (armario == 'V') {
+                    verano++;
+                } else if (armario == 'I') {
+                    invierno++;
+                } else if (armario == 'A') {
+                    entretiempo++;
+                }
+                armario = sc.next().charAt(0);
+            }
+            if (verano > invierno) {
+                System.out.println("VERANO");
+            } else if (verano < invierno) {
+                System.out.println("INVIERNO");
+            } else {
+                System.out.println("EMPATE");
+            }
+
+            armario = sc.next().charAt(0);
+        }
+    }
+
+}
