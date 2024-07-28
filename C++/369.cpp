a93c2c39bffebe26ccc1b861f2bc94719a15c711
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 10:35:55 +0200

    Contando en la Arena

---
+++ C++/369.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+// 369 - Contando en la Arena - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+
+int main()
+{
+
+    int numero = 0;
+    cin >> numero;
+    while (numero != 0) {
+        for(int i = 0; i<numero; i++) {
+            cout << "1" << "";
+        }
+        cout << "\n" << "";
+        cin >> numero;
+    }
+
+}
