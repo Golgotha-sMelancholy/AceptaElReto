74cd6e9c433df648dd6ab2653a2b53026de6d5d4
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 17:16:01 +0200

    Último Dígito del Factorial

---
+++ C++/114.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+// 114 - Último Dígito del Factorial - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+
+int main()
+{
+    int loop = 0;
+    cin >> loop;
+    for (int i = 0; i<loop; i++) {
+        int input = 0;
+        cin >> input;
+        if (input >= 5) {
+            cout << "0" << endl;
+        } else if (input == 4) {
+            cout << "4" << endl;
+        } else if (input == 3) {
+            cout << "6" << endl;
+        } else if (input == 2) {
+            cout << "2" << endl;
+        } else if (input <= 1) {
+            cout << "1" << endl;
+        }
+    }
+
+}
