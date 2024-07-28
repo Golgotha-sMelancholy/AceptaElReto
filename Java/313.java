ff28003a6c26155bf2b7e1aadeadd5b2db789c80
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-28 10:18:32 +0200

    Fin de Mes

---
+++ Java/313.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ *
+ * 313 - Fin de Mes - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bucle = sc.nextInt();
+        for (int i = 1; i <= bucle; i++) {
+            int saldo = sc.nextInt();
+            int cambio = sc.nextInt();
+            if (saldo + cambio >= 0) {
+                System.out.println("SI");
+            } else if (saldo + cambio < 0) {
+                System.out.println("NO");
+            }
+        }
+    }
+}
