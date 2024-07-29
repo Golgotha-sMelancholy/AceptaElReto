ee664a54ccba60c6fed42568aa90b097533df370
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-29 12:41:32 +0200

    Plazas Disponibles en el Mundial

---
+++ C++/710.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+// 710 - Plazas Disponibles en el Mundial - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+
+int main()
+{
+    int loop = 0;
+    cin >> loop;
+    for(int i = 0; i<loop; i++) {
+        int teams = 0;
+        int europe = 0;
+        int asia = 0;
+        int africa = 0;
+        int northAmerica = 0;
+        int southAmerica = 0;
+        int oceania = 0;
+        cin >> teams;
+        cin >> europe;
+        cin >> asia;
+        cin >> africa;
+        cin >> northAmerica;
+        cin >> southAmerica;
+        cin >> oceania;
+        cout << teams-europe-asia-africa-northAmerica-southAmerica-oceania << endl;
+
+    }
+
+}
