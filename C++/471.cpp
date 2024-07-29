e0eda9f706c793f3b170b8ba13b7a610d4f9e470
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-29 17:51:52 +0200

    Buscando el Pinchazo

--- C++/471.cpp
+++ C++/471.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// 471 - Buscando el Pinchazo- Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+// 471 - Buscando el Pinchazo - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
 
 int main()
 {
