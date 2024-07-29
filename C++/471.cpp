2da7cf9ed25a9ba9582e13d4726423b3f7fa4a10
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-29 17:31:09 +0200

    Buscando el Pinchazo

---
+++ C++/471.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// 471 - Buscando el Pinchazo- Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+
+int main()
+{
+    int loop = 0;
+    cin >> loop;
+    for (int i = 0; i<loop; i++) {
+        int positionA = 0;
+        int positionB = 0;
+        cin >> positionA;
+        cin >> positionB;
+        if ((positionA - positionB - 360) % 360 < (positionB - positionA - 360) % 360) {
+                cout << "DESCENDENTE" << endl;
+            } else if ((positionA - positionB - 360) % 360 > (positionB - positionA - 360) % 360) {
+                cout << "ASCENDENTE" << endl;
+            } else {
+                cout << "DA IGUAL" << endl;
+            }
+        }
+}
