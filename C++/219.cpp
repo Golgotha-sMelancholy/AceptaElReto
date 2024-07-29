3fd2ec8731f5aaf13db1013b56962b051c55e1dd
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-07-29 14:39:27 +0200

    La Lotería de la Peña Atlética

---
+++ C++/219.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// 219 - La Lotería de la Peña Atlética - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+
+int main()
+{
+    int loop = 0;
+    cin >> loop;
+    for(int i = 0; i<loop; i++) {
+        int counter = 0;
+        int subloop = 0;
+        cin >> subloop;
+        for (int j = 0; j<subloop; j++) {
+            int number = 0;
+            cin >> number;
+            if (number % 2 == 0){
+                counter++;
+            }
+        }
+        cout << counter << endl;
+    }
+
+}
