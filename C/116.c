c1ced5b718320ba68d02ff827236817b98f9b357
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-08-06 16:08:21 +0200

    ¡Hola Mundo!

---
+++ C/116.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 116 - Hola Mundo - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+
+int main()
+{
+    int loop;
+    scanf("%d", &loop);
+    int i;
+    for (i = 0;i<loop; i++) {
+        printf("Hola mundo.\n");
+    }
+    return 0;
+}
