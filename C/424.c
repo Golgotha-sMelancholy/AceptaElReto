23efe06852d372f7fe076551572407e25985a6a1
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-08-06 20:06:14 +0200

    Ahorro Infantil

---
+++ C/424.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 424 - Ahorro Infantil - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+int main()
+{
+    int loop;
+    scanf("%d", &loop);
+    while (loop != 0) {
+        int i;
+        int result = 0;
+        int maxResult = 0;
+        for (i = 0; i<loop; i++) {
+            int n = 0;
+            scanf("%d", &n);
+            result += n;
+            if (result > maxResult) {
+                maxResult = result;
+            }
+        }
+        printf("%d %d\n", result, maxResult);
+        scanf("%d", &loop);
+    }
+    return 0;
+}
