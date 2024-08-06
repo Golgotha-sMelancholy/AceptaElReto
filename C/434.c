289755739b42d995a2763bf27eea1c054e77d81a
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-08-06 21:48:22 +0200

    Romance en el Palomar

---
+++ C/434.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 434 - Romance en el Palomar - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+int main()
+{
+    int loop;
+    scanf("%d", &loop);
+    int i;
+    int dovesSleeping = 0;
+    int ableBoxes = 0;
+    for (i = 0; i<loop; i++) {
+        scanf("%d", &dovesSleeping);
+        scanf("%d", &ableBoxes);
+        if (dovesSleeping>ableBoxes) {
+            printf("PRINCIPIO\n");
+        } else {
+            printf("ROMANCE\n");
+        }
+    }
+    return 0;
+}
