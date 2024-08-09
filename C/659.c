2b120f2fd0b83daf2fddafb1e3f11d325f91b475
Golgotha-sMelancholy <iamtheanguera@gmail.com>
2024-08-09 16:36:20 +0200

    Números Jeroglíficos

---
+++ C/659.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 659 - Números Jeroglíficos - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+int main()
+{
+    int number = 0;
+    scanf("%d", &number);
+    while (number != 0) {
+        while (number >= 1000000) {
+            number -= 1000000;
+            printf("H");
+        }
+        while (number >= 100000) {
+            number -= 100000;
+            printf("R");
+        }
+        while (number >= 10000) {
+            number -= 10000;
+            printf("D");
+        }
+        while (number >= 1000) {
+            number -= 1000;
+            printf("F");
+        }
+        while (number >= 100) {
+            number -= 100;
+            printf("C");
+        }
+        while (number >= 10) {
+            number -= 10;
+            printf("G");
+        }
+        while (number >= 1) {
+            number -= 1;
+            printf("T");
+        }
+
+        printf("\n");
+        scanf("%d", &number);
+    }
+    return 0;
+}
