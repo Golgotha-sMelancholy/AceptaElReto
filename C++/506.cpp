4b0ac10eccc23d2c558428de4303c90e50cf7e78
Iván <iamtheanguera@gmail.com>
2024-09-26 16:26:57 +0200

    Tensión Descompensada

--- C++/506.cpp
+++ C++/506.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// 506 - Tensión Descompensada - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+// 506 - Tensión Descompensada - Iván - https://github.com/wildfireOfMine
 
 int main()
 {
