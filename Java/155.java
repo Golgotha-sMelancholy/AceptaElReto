50cb083560f4e20f721268689129890781037079
Iván <iamtheanguera@gmail.com>
2024-09-26 17:06:12 +0200

    Perímetro de un Rectángulo

--- Java/155.java
+++ Java/155.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /**
  *
- * 155 - Perímetro de un Rectángulo - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ * 155 - Perímetro de un Rectángulo - Iván - https://github.com/wildfireOfMine
  */
 public class Main {
 
