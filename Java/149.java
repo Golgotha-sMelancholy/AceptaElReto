25df01152eb1d1ca3e013d8509dd8bd3efa509b0
Iván <iamtheanguera@gmail.com>
2024-09-26 17:05:40 +0200

    San Fermines

--- Java/149.java
+++ Java/149.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 /**
  *
- * 149 - San Fermines - Golgotha's Melancholy - https://github.com/Golgotha-sMelancholy
+ * 149 - San Fermines - Iván - https://github.com/wildfireOfMine
  */
 public class Main {
 
